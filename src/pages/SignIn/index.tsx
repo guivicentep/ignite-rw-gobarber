135be2492f91f7a1c0d7cc9fd16828f281054997
guivicentep <guivicente.gvp@hotmail.com>
2020-07-15 21:29:19 -0300

    Commit 1507

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
+import { FiLogIn } from 'react-icons/fi';
+
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -8,6 +13,22 @@ const SignIn: React.FC = () => (
   <Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faça seu logon</h1>
+
+        <Input name="email" placeholder="E-mail" />
+
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <Button type="submit">Entrar</Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+      <a href="sdadsa">
+        <FiLogIn />
+        Criar conta
+      </a>
     </Content>
     <Background />
   </Container>
