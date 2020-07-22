f1b904a0eab58d139956dfc306562fe29fbeefaf
guivicentep <guivicente.gvp@hotmail.com>
2020-07-21 21:12:44 -0300

    Commit pós Autenticação

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,11 +1,10 @@
 import React, { useCallback, useRef } from 'react';
-
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
-
 import { Form } from '@unform/web';
-
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
@@ -14,10 +13,11 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  const formRef = useRef(null);
+  const formRef = useRef<FormHandles>(null);
 
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -30,7 +30,8 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
