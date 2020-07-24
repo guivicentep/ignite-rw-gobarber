f6ffe37dab1c864a04e3f8383a9a8bc74bbe956f
guivicentep <guivicente.gvp@hotmail.com>
2020-07-24 10:28:21 -0300

    Finished SignIn and SignUp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -8,9 +9,9 @@ import Dashboard from '../pages/Dashboard';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exact component={SignUp} />
+    <Route path="/signup" component={SignUp} />
 
-    <Route path="/dashboard" exact component={Dashboard} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
